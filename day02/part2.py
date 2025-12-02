381e74c3eda7d0b8c9167b0349b13a5bc4c5d4cf
Peter Gürtler <peter.guertler@ausy.ch>
2025-12-02 07:30:55 +0100

    Day 2 part 2

--- day02/part2.py
+++ day02/part2.py
@@ -4,22 +4,19 @@ sys.path.append('../lib')
 from pmg import *
 
 def is_invalid(s):
-    print("----------------")
-    print(s)
     ls = len(s)
-    print(int(len(s)/2))
     for i in range(1, 1 + int(len(s)/2)):
         if ls % i == 0:
             first = s[:i]
-            for j in [1:i]:
-                sub = s[j
-        print(i)
+            all_match = True
+            for j in range(1, int(ls/i)):
+                item = s[j*i: (j+1) * i]
+                if item != first:
+                    all_match = False
+                    break
+            if all_match:
+                return True
     return False
-    #if len(s) % 2 == 0:
-    #    lh = int(len(s)/2)
-    #    if s[:lh] == s[lh:]:
-    #        return True
-    #return False
 
 with open(sys.argv[1]) as f:
     lines = f.read().split(",")
