bbc9db6f5d15488001d94ff437171ccd008963a4
Peter Gürtler <coder@sonensei.ch>
2025-12-09 06:34:58 +0100

    Started day 9 part 2

--- day09/part2.py
+++ day09/part2.py
@@ -16,10 +16,27 @@ class Grid:
     def get(self, x, y):
         return self.data[x][y]
 def connect(grid, r1, r2):
-    r = r1
-    dx = si
-    while r[0] != r2[0] and r[1] != r2[1]:
-        r 
+    print("Connect", r1, r2)
+    x1 = min(r1[0], r2[0])
+    x2 = max(r1[0], r2[0])
+    y1 = min(r1[1], r2[1])
+    y2 = max(r1[1], r2[1])
+
+    print(x1, x2, y1, y2) 
+    dx = 1 if x2 > x1 else 0
+    dy = 1 if y2 > y1 else 0
+    x = x1
+    y = y1
+    print(dx, dy, x, y)
+    while x != x2 or y != y2:
+        if grid.get(y, x) == ".":
+            grid.set(y, x, "X")
+            print("Going green", x, y)
+        else:
+            print("Not greening", x, y, grid.get(y, x))
+        x += dx
+        y += dy
+
 with open(sys.argv[1]) as f:
     lines = f.read().splitlines()
     grid = Grid(12, 12, ".")
@@ -34,7 +51,7 @@ with open(sys.argv[1]) as f:
     connect(grid, red[-1], red[0])
 
     for r in grid.data:
-        print(r)
+        print("".join(r))
     #max_area = -1
     #for r1 in red:
     #    for r2 in red:
